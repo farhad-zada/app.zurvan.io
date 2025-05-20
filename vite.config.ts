561f170688baf35c474903678cd57b0e97914fae
Farhad Zada <farhad.szd@gmail.com>
2025-05-21 02:36:37 +0400

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    outDir: "../dist"
+    outDir: "./dist"
   },
   base: "./",
 })
